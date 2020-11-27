94c0cf4dd9d95f9998b8750560cc0498ce355805
Renzil DSouza <renzil@gmail.com>
2020-11-27 17:02:10 +0530

    Removing unused href in BotLink

--- app/components/BotListItem/index.js
+++ app/components/BotListItem/index.js
@@ -27,14 +27,13 @@ function BotListItem({ item }) {
   const content = (
     <Wrapper onClick={() => history.push(`/bot/${item.id}`)}>
       <Avatar
-        image={`https://ui-avatars.com/api/?name=${
-          item.name
-        }&color=555&background=${PRIMARY_COLOR.replace('#', '')}`}
+        image={`https://ui-avatars.com/api/?name=${item.name}\
+&color=555&background=${PRIMARY_COLOR.replace('#', '')}`}
         width="40px"
         height="40px"
       />
       &emsp;
-      <BotLink href={`/bot/${item.id}`}>{item.name}</BotLink>
+      <BotLink>{item.name}</BotLink>
     </Wrapper>
   );
 
